696623b64513f202c333e2c34d276b0c517b1c46
Eram-khan786 <eram22@navgurukul.org>
2025-12-31 15:08:58 +0530

    First commit

---
+++ src/test/java/com/example/sample_java_assignment_app/SpringbootSampleAppApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.sample_java_assignment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringbootSampleAppApplicationTests {
+
+	@Test
+	void testLoginMethod() {
+        SpringbootSampleAppApplication obj=new SpringbootSampleAppApplication();
+         String res=obj.login("Mute");
+         assertEquals("Please input your credentials for login...",res);
+	}
+
+
+    void testRegisterUser() {
+        SpringbootSampleAppApplication obj=new SpringbootSampleAppApplication();
+        String res=obj.register("Mute");
+        assertEquals("Please follow registration process...",res);
+    }
+
+}
